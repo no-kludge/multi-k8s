bbb193f016cfff48f5f36a7bbdedd4fc6d714de1
Sagar <catchsagar@gmail.com>
2020-05-01 19:32:22 +0530

    updated CI script files with correct deployment values

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t catchsagar/multi-k8s-client:latest -t catchsagar/multi-k8s-client:$SHA -f ./client/Dockerfile ./client
+docker build -t catchsagar/multi-k8s-server:latest -t catchsagar/multi-k8s-server:$SHA -f ./server/Dockerfile ./server
+docker build -t catchsagar/multi-k8s-worker:latest -t catchsagar/multi-k8s-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push catchsagar/multi-k8s-client:latest
+docker push catchsagar/multi-k8s-server:latest
+docker push catchsagar/multi-k8s-worker:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push catchsagar/multi-k8s-client:$SHA
+docker push catchsagar/multi-k8s-server:$SHA
+docker push catchsagar/multi-k8s-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=catchsagar/multi-k8s-server:$SHA
+kubectl set image deployments/client-deployment client=catchsagar/multi-k8s-client:$SHA
+kubectl set image deployments/worker-deployment worker=catchsagar/multi-k8s-worker:$SHA
